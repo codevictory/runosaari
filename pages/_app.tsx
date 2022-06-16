b7ee46401df43039e93cc4ade9f6b6bb5919a4e7
veikko <veikko@lintujarvi.fi>
2022-06-16 19:56:24 +0300

    Refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
+import { Footer } from './layout/Footer';
+import { Header } from './layout/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
