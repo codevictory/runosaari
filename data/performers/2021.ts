29e7d5a01beeb6dd15c1d9624bd68803f9f5b489
veikko <veikko@lintujarvi.fi>
2022-06-18 11:42:21 +0300

    2022 performer data added

--- data/performers/2021.ts
+++
@@ -1,7 +0,0 @@
-import type Performer from '../../types/Performer';
-
-const Performers: Array<Performer> = [
-    { id: 1, name: "Markku Pääskynen", paragraphs: ["Markku Pääskynen (s. 1973) on Helsingissä asuva kirjailija. Hän on koulutukseltaan filosofian maisteri ja julkaissut romaaneja, novelleja ja esseitä. Lisäksi hän on suomentanut lyriikkaa mm. nykykreikasta. Pääskynen on ollut kahdesti Finlandia-ehdokkaana, saanut Kalevi Jäntin palkinnon sekä Savonia-palkinnon."], imagePath: "/performers/2022/markku_paaskynen.jpg", showDesc: false},
-];
-
-export default Performers;
