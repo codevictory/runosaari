9edd9618659a43fdd9c1e958bdf1928f0b9754ea
veikko <veikko@lintujarvi.fi>
2022-06-20 21:27:44 +0300

    Lead paragraph

---
+++ pages/components/Lead.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '../../styles/Lead.module.scss';
+
+const Lead = () => {
+  return (
+    <div className={styles.leadContainer}>
+      <h2 className={styles.leadTitle}>
+        <span>~ 20.-23.7. Livonsaari & Velkuanmaa ~</span>
+        <br />
+        Eksymisretki omaan luontoosi, metsänpeiton suojaan!
+      </h2>
+      <p className={styles.leadText}>
+        Runosaari on poikkitaiteellinen runofestivaali Livonsaaren, Palvan ja
+        Velkuanmaan saarissa. Festivaali järjestetään toista kertaa
+        20.–23.7.2022.
+      </p>
+      <p className={styles.leadText}>
+        Tapahtuma kutsuu Naantalin saaristoon joukon valovoimaisia ja
+        monialaisia taiteen tekijöitä. Ohjelmassa runous yhdistyy elävään
+        musiikkiin ja erilaisiin taiteellisiin työpajoihin.
+      </p>
+      <p className={styles.leadText}>
+        Lumi ja jää vähenee, vedenpinta nousee ja pandemian aallot viistävät
+        meidänkin rantojamme. Peurojen kurittaman monimuotoisuuden keskelle
+        nousee Runosaari ihmettelemään ihmisen ja muun luonnon olemista taiteen
+        ja ilmaisun keinoin.
+      </p>
+      <p className={styles.leadText}>
+        Runosaaren päätapahtuman ajan perjantaina 22.7. Sinervon talolla pitää
+        majaa kiehtovien kirjojen tori.
+      </p>
+    </div>
+  );
+};
+
+export default Lead;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,13 @@ import Info from './info';
 import Archive from './archive';
 import Safety from './safety';
 import styles from '../styles/Index.module.scss';
+import Lead from './components/Lead';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.indexContainer}>
+      <h1>Runosaari 2022</h1>
+      <Lead />
       <Performers />
       <Program />
       <Info />
