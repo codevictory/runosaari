1f4564884cd81ddaf0670c7c089d46dfdf916cd0
veikko <veikko@lintujarvi.fi>
2022-07-03 22:03:24 +0300

    Update Archive page

--- pages/archive.tsx
+++ pages/archive.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import shared from '../styles/Shared.module.scss';
+import styles from '../styles/Archive.module.scss';
 
 const Archive = () => {
   return (
-    <section className={shared.page}>
+    <section className={shared.page + ' ' + styles.archivePage}>
       <h1>Arkisto</h1>
 
       <i>Lisätietoja tulossa myöhemmin!</i>
