b1839ce31f913eba066250c9ea1b86c89abfbae7
Veikko Lintujärvi <veikko@lintujarvi.fi>
2023-05-26 18:12:24 +0300

    Update program title styling

--- pages/program.tsx
+++ pages/program.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FiExternalLink } from 'react-icons/fi';
 import shared from '../styles/Shared.module.scss';
-import program from '../styles/Program.module.scss';
+import styles from '../styles/Program.module.scss';
 
 interface DayToggles {
   wed: boolean;
@@ -30,25 +30,37 @@ const Program = () => {
     <section className={shared.page}>
       <h1 id='program-start'>Ohjelma</h1>
 
-      <h2>
-        20.7. Laituri{' '}
-        <a href='https://www.cafelaituri.fi'>
-          <FiExternalLink fontSize={20} />
-        </a>
+      <h2 className={styles.programTitle}>
+        <div>20.7.</div>
+        <div>
+          <span className={styles.placeName}>Laituri</span>
+          <a href='https://www.cafelaituri.fi'>
+            <FiExternalLink fontSize={20} />
+          </a>
+        </div>
+        <div />
       </h2>
       <p>Café Laituri, Voiponlahdentie 37, Palva</p>
-      <h2>
-        21.7. Vaihela{' '}
-        <a href='https://vaihela.fi/'>
-          <FiExternalLink fontSize={20} />
-        </a>
+      <h2 className={styles.programTitle}>
+        <div>21.7.</div>
+        <div>
+          <span className={styles.placeName}>Vaihela</span>
+          <a href='https://vaihela.fi/'>
+            <FiExternalLink fontSize={20} />
+          </a>
+        </div>
+        <div />
       </h2>
       <p>Saaristohotelli Vaihela, Velkuanmaantie 168, Palva</p>
-      <h2>
-        22.7. Sinervo{' '}
-        <a href='https://www.prosinervo.com/'>
-          <FiExternalLink fontSize={20} />
-        </a>
+      <h2 className={styles.programTitle}>
+        <div>22.7.</div>
+        <div>
+          <span className={styles.placeName}>Sinervo</span>
+          <a href='https://www.prosinervo.com/'>
+            <FiExternalLink fontSize={20} />
+          </a>
+        </div>
+        <div />
       </h2>
       <p style={{ marginBottom: 50 }}>
         Sinervon talo, Sauniementie 5, Teersalo
