340855c83e7abf4a41c9ae83222891af715eeaa1
Veikko Lintujärvi <veikko@lintujarvi.fi>
2024-05-16 19:13:44 +0300

    Typo fix

--- pages/components/Lead.tsx
+++ pages/components/Lead.tsx
@@ -6,7 +6,7 @@ const Lead = () => {
     <div className={styles.leadContainer}>
       <h2 className={styles.leadTitle}>
         <span className={styles.dateAndPlace}>
-          7. ja 14.6. Naantalin Saaristo
+          7. ja 14.6. Naantalin saaristo
         </span>
         Vuonna 2024 saarifestivaali tuo runoutta ja musiikkia kahteen kesäkuun perjantaihin
       </h2>
