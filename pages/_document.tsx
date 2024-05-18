8abe2a1f35ea942ab501b5416a34af16064e8d14
Veikko Lintujärvi <veikko@lintujarvi.fi>
2024-05-18 18:52:08 +0300

    Fix open graph config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
           <meta property="og:title" content="Runosaari" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.runosaari.net/" />
-          <meta property="og:image:url" content="runosaari-logo_small.jpg" />
+          <meta property="og:image:url" content="https://www.runosaari.net/runosaari-logo_small.jpg" />
           <meta property="og:description" content="Runosaari 7. ja 14.6. Naantalin saaristo. Vuonna 2024 saarifestivaali tuo runoutta ja musiikkia kahteen kesäkuun perjantaihin" />
           <script
             async
