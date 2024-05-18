8efdd385d387863ef37e5b390e3268ff85fc19e7
Veikko Lintujärvi <veikko@lintujarvi.fi>
2024-05-18 18:48:25 +0300

    OG config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,11 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css?family=Crimson+Text'
             rel='stylesheet'
           />
+          <meta property="og:title" content="Runosaari" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.runosaari.net/" />
+          <meta property="og:image:url" content="runosaari-logo_small.jpg" />
+          <meta property="og:description" content="Runosaari 7. ja 14.6. Naantalin saaristo. Vuonna 2024 saarifestivaali tuo runoutta ja musiikkia kahteen kesäkuun perjantaihin" />
           <script
             async
             defer
