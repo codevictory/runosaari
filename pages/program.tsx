3fb818f51fd3c943df681921e023d30ad3443cdb
Veikko Lintujärvi <veikko@lintujarvi.fi>
2025-05-29 21:20:13 +0300

    Updated program

--- pages/program.tsx
+++ pages/program.tsx
@@ -24,54 +24,37 @@ const Program = () => {
           Cafe Laituri
         </a>
       </h2>
+      <i>Ohjelma täydentyy...</i>
+      <hr className={styles.programHr} />
       <p className={styles.programTimeAndPlace}>
         Perjantai 13.6. klo 18-21 <span className={styles.locationName}>Velkuanmaa</span>
       </p>
       <h2 className={styles.placeTitle}>
-        <a href='https://www.cafelaituri.fi'>
+        <a href='https://www.vaihela.fi/'>
           <span className={styles.placeName}>Saaristohotelli Vaihela</span>
         </a>
       </h2>
+      <ul className={styles.performerList}>
+        <li>Kauko Röyhkä & Severi Pyysalo</li>
+      </ul>
+      <i>Ohjelma täydentyy...</i>
+      <hr className={styles.programHr} />
       <p className={styles.programTimeAndPlace}>
         Lauantai 14.6. klo 15-20 <span className={styles.locationName}>Teersalo</span>
       </p>
       <h2 className={styles.placeTitle}>
-        <a href='https://www.cafelaituri.fi'>
+        <a href='https://prosinervo.com/'>
           <span className={styles.placeName}>Sinervon talo</span>
         </a>
       </h2>
-
-      {/* <ul className={styles.performerList}>
-        <li>Äyräs</li>
-        <li>Hanna Storm</li>
-        <li>Juha Rautio</li>
-        <li>Katariina Vuorinen<br />& Ilkka Turta</li>
-      </ul>
-      <i>Ohjelma täydentyy...</i>
-      <h2 className={styles.programTitle}>
-        <span>14.6.</span>
-        <a href='https://www.cafelaituri.fi'>
-          <span className={styles.placeName}>Laituri</span>
-          <FiExternalLink fontSize={20} />
-        </a>
-        <span>pe</span>
-      </h2>
-      <span className={styles.locationAddress}>
-        Saaristohotelli Vaihela, Velkuanmaantie 168, Velkuanmaa
-      </span>
-      <span className={styles.timeSpan}>18:00 - 21:00</span>
-
       <ul className={styles.performerList}>
-        <li>Juha Kulmala + Positroninen runo-orkesteri </li>
-        <li>Terhi Forssén</li>
-        <li>Katariina Vuorinen & Björn</li>
-        <li>Risto Oikarinen</li>
-        <li>Emmi Ketonen</li>
+        <li>Uhrijuhla</li>
       </ul>
       <i>Ohjelma täydentyy...</i>
+
       <hr className={styles.programHr} />
 
-      <i className={shared.moreInfoLaterText}>Lisätietoja tulossa myöhemmin...</i> */}
+      <i className={shared.moreInfoLaterText}>Lisätietoja tulossa myöhemmin...</i>
     </section>
   );
 };
